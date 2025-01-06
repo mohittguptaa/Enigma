f438fa55d89817c56234779cbab87327326753bb
Mohit Gupta <128838835+mohittguptaa@users.noreply.github.com>
2025-01-06 23:14:55 +0530

    Update gimini.js

--- src/config/gimini.js
+++ src/config/gimini.js
@@ -9,7 +9,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash-exp",
   });
   
   const generationConfig = {
